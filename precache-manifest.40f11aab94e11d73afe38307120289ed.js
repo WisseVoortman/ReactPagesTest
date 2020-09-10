623eada925cfb1ecde820e3c8ecce8796cfb108a
Wisse Voortman <w_voortman@hotmail.com>
2020-09-10 13:50:07 +0200

    Updates

---
+++ precache-manifest.40f11aab94e11d73afe38307120289ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42adf4da2f321690c1e8234674137edc",
+    "url": "/reactpagestest/index.html"
+  },
+  {
+    "revision": "e9c35629130ac3bb0de1",
+    "url": "/reactpagestest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b8cb8fff74d5c81c918",
+    "url": "/reactpagestest/static/js/2.a7e62782.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactpagestest/static/js/2.a7e62782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c35629130ac3bb0de1",
+    "url": "/reactpagestest/static/js/main.477abc50.chunk.js"
+  },
+  {
+    "revision": "392a8d9b84d7a4fc4717",
+    "url": "/reactpagestest/static/js/runtime-main.5cf1c82e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactpagestest/static/media/logo.5d5d9eef.svg"
+  }
+]);
